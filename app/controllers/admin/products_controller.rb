5aaba319ddccf4bfe8d0990c188b862a06ce5a3f
yuanchang <759337828@qq.com>
2020-06-17 21:24:04 +0800

    implement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,45 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
+
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:success] = "添加商品成功"
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      flash[:success] = "更新商品成功"
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
   end
 end
