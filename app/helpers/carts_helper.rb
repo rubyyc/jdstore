9ace91bf1067b3099bb770a1feda41030f96bf22
yuanchang <759337828@qq.com>
2020-06-18 17:24:07 +0800

    implement cart_total_price function

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,14 @@
 module CartsHelper
+  def render_cart_total_price(cart)
+    # sum = 0
+    #
+    # cart.cart_items.each do |item|
+    #   if item.product.price.present?
+    #     sum += item.product.price * item.quantity
+    #   end
+    # end
+    # sum
+    cart.total_price
+  end
+
 end
