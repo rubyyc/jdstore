57513751a3505637b6ac80f0347e5c9b3e70d45b
yuanchang <759337828@qq.com>
2020-06-18 16:30:02 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
+
+  resources :carts
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
