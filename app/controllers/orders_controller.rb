f077376c51d3fd556722049614dcb3ec08a3e9ce
yuanchang <759337828@qq.com>
2020-06-20 18:10:26 +0800

    add payment_method function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,10 +14,10 @@ class OrdersController < ApplicationController
         product_list.product_price = cart_item.product.price
         product_list.quantity = cart_item.quantity
         product_list.save
-        flash[:notice] = "yyyy"
+        # flash[:notice] = "yyyy"
         # binding.pry
       end
-      flash[:notice] = "xxx"
+      # flash[:notice] = "xxx"
       redirect_to order_path(@order.token)
     else
       false[:warning] ="Order Check Failed...Please try again."
@@ -31,7 +31,25 @@ class OrdersController < ApplicationController
     @product_lists = @order.product_lists
   end
 
+  def pay_with_alipay
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("alipay")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "使用支付宝付款成功"
+  end
+
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.pay!
+
+    redirect_to order_path(@order.token), notice: "使用微信付款成功"
+  end
+
   private
+
+
 
   def order_params
     params.require(:order).permit(:billing_address, :billing_name, :shipping_address, :shipping_name)
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,12 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
