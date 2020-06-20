bc2292d26b17b3b72738cf0111c73890841085eb
yuanchang <759337828@qq.com>
2020-06-20 16:14:15 +0800

    git commit -m "touch app/views/carts/checkout.html.erb"

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :orders
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
